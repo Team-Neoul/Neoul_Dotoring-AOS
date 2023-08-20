0016f266e36d5c0a2b6e900a7f23de7355592ee1
HyeonjiJang <jhg050908@gmail.com>
2023-08-20 23:38:34 +0900

    feat: Theme 설정

--- app/src/main/java/com/example/myapplication/ui/theme/Type.kt
+++ app/src/main/java/com/example/myapplication/ui/theme/Type.kt
@@ -1,34 +1,23 @@
 package com.example.myapplication.ui.theme
 
-import androidx.compose.material3.Typography
-import androidx.compose.ui.text.TextStyle
+import androidx.compose.material.Typography
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.unit.sp
+import com.example.myapplication.R
 
 // Set of Material typography styles to start with
-val Typography = Typography(
-    bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 16.sp,
-        lineHeight = 24.sp,
-        letterSpacing = 0.5.sp
-    )
-    /* Other default text styles to override
-    titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 22.sp,
-        lineHeight = 28.sp,
-        letterSpacing = 0.sp
-    ),
-    labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Medium,
-        fontSize = 11.sp,
-        lineHeight = 16.sp,
-        letterSpacing = 0.5.sp
-    )
-    */
+val NanumSquare = FontFamily(
+    Font(R.font.nanumsquare_light, FontWeight.Light),
+    Font(R.font.nanumsquare_regular, FontWeight.Normal),
+    Font(R.font.nanumsquare_bold, FontWeight.Bold),
+    Font(R.font.nanumsquare_extrabold, FontWeight.ExtraBold),
+    Font(R.font.nanumsquare_ac_light, FontWeight.Light),
+    Font(R.font.nanumsquare_ac_regular, FontWeight.Normal),
+    Font(R.font.nanumsquare_ac_bold, FontWeight.Bold),
+    Font(R.font.nanumsquare_ac_extrabold, FontWeight.ExtraBold)
+)
+
+val DotoringTypography = Typography(
+    defaultFontFamily = NanumSquare
 )
