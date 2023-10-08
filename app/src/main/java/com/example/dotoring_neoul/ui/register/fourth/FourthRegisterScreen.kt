68746bf62470d8390075f6b3d2c32a5bcaa48554
HyeonjiJang <jhg050908@gmail.com>
2023-10-08 22:10:21 +0900

    [#49] fix: 멘토 회원 가입 네번째 화면 - 텍스트 필드 영역 고정되는 문제 해결

--- app/src/main/java/com/example/dotoring_neoul/ui/register/fourth/FourthRegisterScreen.kt
+++ app/src/main/java/com/example/dotoring_neoul/ui/register/fourth/FourthRegisterScreen.kt
@@ -2,8 +2,12 @@ package com.example.dotoring_neoul.ui.register.fourth
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
@@ -12,7 +16,10 @@ import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.foundation.text.KeyboardActionScope
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.ContentAlpha
 import androidx.compose.material.ExperimentalMaterialApi
+import androidx.compose.material.LocalContentColor
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.material.TextFieldDefaults
 import androidx.compose.material.TextFieldDefaults.indicatorLine
@@ -22,10 +29,12 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.tooling.preview.Preview
@@ -63,7 +72,9 @@ fun FourthRegisterScreen(
 
         Spacer(modifier = Modifier.size(30.dp))
 
-        Column {
+        Column(
+            modifier = Modifier.padding(start = 30.dp, end = 30.dp)
+        ) {
             Text(text = stringResource(id = R.string.register_A))
 
             Spacer(modifier = Modifier.size(10.dp))
@@ -78,10 +89,15 @@ fun FourthRegisterScreen(
                     } else {
                         registerFourthViewModel.updateNextButtonState(false)
                     }
-                                },
+                },
                 onDone = {
                     focusManager.clearFocus()
-                }
+                },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .clip(RoundedCornerShape(15.dp))
+                    .background(colorResource(R.color.grey_200))
+                    .padding(16.dp)
             )
 
             Spacer(modifier = Modifier.height(1.dp))
@@ -91,38 +107,38 @@ fun FourthRegisterScreen(
                     stringResource(id = R.string.register4_error_condition_not_satisfied)
 
                 } else {
-                                                                         ""
-                                                                         },
+                    ""
+                },
                 modifier = Modifier
                     .padding(start = 2.dp, top = 3.dp),
                 color = Color(0xffff7B7B),
                 fontSize = 10.sp
             )
-
-            Spacer(modifier = Modifier.weight(5f))
-
-            RegisterScreenNextButton(
-                onClick = {
-                    val mentoInfo = MentoInformation(
-                        company = mentoInformation.company,
-                        careerLevel = mentoInformation.careerLevel,
-                        job = mentoInformation.job,
-                        major = mentoInformation.major,
-                        employmentCertification = mentoInformation.employmentCertification,
-                        graduateCertification = mentoInformation.graduateCertification,
-                        nickname = mentoInformation.nickname,
-                        introduction = registerFourthUiState.mentorIntroduction
-                    )
-                    navController.currentBackStackEntry?.savedStateHandle?.set(
-                        key = "mentoInfo",
-                        value = mentoInfo
-                    )
-                    navController.navigate(AuthScreen.Register5.route)
-                },
-                enabled = registerFourthUiState.nextButtonState)
-
-            Spacer(modifier = Modifier.weight(4f))
         }
+
+        Spacer(modifier = Modifier.weight(5f))
+
+        RegisterScreenNextButton(
+            onClick = {
+                val mentoInfo = MentoInformation(
+                    company = mentoInformation.company,
+                    careerLevel = mentoInformation.careerLevel,
+                    job = mentoInformation.job,
+                    major = mentoInformation.major,
+                    employmentCertification = mentoInformation.employmentCertification,
+                    graduateCertification = mentoInformation.graduateCertification,
+                    nickname = mentoInformation.nickname,
+                    introduction = registerFourthUiState.mentorIntroduction
+                )
+                navController.currentBackStackEntry?.savedStateHandle?.set(
+                    key = "mentoInfo",
+                    value = mentoInfo
+                )
+                navController.navigate(AuthScreen.Register5.route)
+            },
+            enabled = registerFourthUiState.nextButtonState)
+
+        Spacer(modifier = Modifier.weight(4f))
     }
 }
 
@@ -130,6 +146,35 @@ fun FourthRegisterScreen(
  * 회원 가입 네번째 화면 텍스트필드 composable
  */
 @OptIn(ExperimentalMaterialApi::class)
+@Composable
+fun RoundedCornerTextField(
+    value: String,
+    onValueChange: (String) -> Unit,
+    modifier: Modifier = Modifier,
+    hintText: String = "",
+    textStyle: TextStyle = MaterialTheme.typography.body1,
+    maxLines: Int = 5,
+    onDone: (KeyboardActionScope.() -> Unit)?
+) {
+    BasicTextField(
+        value = value,
+        onValueChange = onValueChange,
+        textStyle = textStyle,
+        maxLines = maxLines,
+        decorationBox = {innerTextField ->
+            Box(modifier = modifier) {
+                if(value.isEmpty()) {
+                    Text(
+                        text = hintText,
+                        color = LocalContentColor.current.copy(alpha = ContentAlpha.medium)
+                    )
+                }
+                innerTextField()
+            }
+        }
+    )
+}
+/*@OptIn(ExperimentalMaterialApi::class)
 @Composable
 fun RoundedCornerTextField(
     value: String,
@@ -171,7 +216,8 @@ fun RoundedCornerTextField(
         enabled = enabled,
         singleLine = singleLine,
         keyboardOptions = KeyboardOptions(imeAction = ImeAction.Done),
-        keyboardActions = KeyboardActions(onDone = onDone)
+        keyboardActions = KeyboardActions(onDone = onDone),
+        maxLines = 5
     ) {
         TextFieldDefaults.TextFieldDecorationBox(
             value = value,
@@ -195,7 +241,7 @@ fun RoundedCornerTextField(
             )
         )
     }
-}
+}*/
 
 /**
  * 회원 가입 네번째 화면 미리보기
