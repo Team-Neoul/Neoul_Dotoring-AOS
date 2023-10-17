9bb0580d9a4380a7c463e72ebd6363124675204c
HyeonjiJang <jhg050908@gmail.com>
2023-10-18 01:17:54 +0900

    [#53] refactor: BottomSheetSelectedData에서 하드코딩된 값을 소프트코딩으로 변경

--- app/src/main/java/com/example/dotoring_neoul/ui/util/bottomsheet/BottomSheetSelectedData.kt
+++ app/src/main/java/com/example/dotoring_neoul/ui/util/bottomsheet/BottomSheetSelectedData.kt
@@ -18,30 +18,49 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.VisualTransformation
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.dotoring.R
+import com.example.dotoring_neoul.ui.theme.DotoringTheme
 
 @OptIn(ExperimentalMaterialApi::class)
 @Composable
-fun SelectedData ( selectedData:String, onClick: () -> Unit ) {
+fun BottomSheetSelectedData(selectedData:String, onClick: () -> Unit ) {
     val interactionSource = remember { MutableInteractionSource() }
+    
+    val backgroundColor = colorResource(R.color.white)
+    val radius = 20.dp
+    val backgroundWidth = 380.dp
+    val backgroundHeight = 36.dp
+
+    val fontSize = 15.sp
+    val fontWeight = FontWeight.Bold
+    val contentPadding = TextFieldDefaults.textFieldWithoutLabelPadding(
+        start = 20.dp, top = 1.dp, end = 1.dp, bottom = 3.dp
+    )
+
+    val iconColor = Color(0xffA5A5A5)
+    val iconSize = 15.dp
 
     Box(
         contentAlignment = Alignment.CenterEnd
     ) {
-
         BasicTextField(
             value = selectedData,
             onValueChange = { } ,
             modifier = Modifier
                 .background(
-                    color = colorResource(id = R.color.white),
-                    shape = RoundedCornerShape(20.dp)
+                    color = backgroundColor,
+                    shape = RoundedCornerShape(radius)
                 )
-                .size(width = 380.dp, height = 36.dp),
-            textStyle = LocalTextStyle.current.copy(fontSize = 15.sp),
+                .size(width = backgroundWidth, height = backgroundHeight),
+            textStyle = LocalTextStyle.current.copy(
+                fontSize = fontSize,
+                fontWeight = fontWeight
+            ),
             enabled = false,
             readOnly = true,
         ) {
@@ -52,20 +71,29 @@ fun SelectedData ( selectedData:String, onClick: () -> Unit ) {
                 singleLine = true,
                 visualTransformation = VisualTransformation.None,
                 interactionSource =  interactionSource,
-                contentPadding = TextFieldDefaults.textFieldWithoutLabelPadding(
-                    start = 20.dp, top = 1.dp, end = 1.dp, bottom = 3.dp
-                )
+                contentPadding = contentPadding
             )
         }
 
-        IconButton (onClick = onClick)
-        {
+        IconButton(onClick = onClick) {
             Icon(
                 painter = painterResource(id = R.drawable.ic_filter_cancel),
-                contentDescription = "취소 버튼",
-                modifier = Modifier.size(15.dp),
-                tint = Color(0xffA5A5A5)
+                contentDescription = "선택 취소 버튼",
+                modifier = Modifier.size(iconSize),
+                tint = iconColor
             )
+        }
+    }
+}
+
+@Preview(
+    showBackground = true,
+    backgroundColor = 0x8BD045)
+@Composable
+private fun RegisterScreenPreview() {
+    DotoringTheme {
+        BottomSheetSelectedData(selectedData = "테스트") {
+            
         }
     }
 }
