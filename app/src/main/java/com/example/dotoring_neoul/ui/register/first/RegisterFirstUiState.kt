af20f6d6fc034a278ac0aacfc67c4ca0bc0d4d40
HyeonjiJang <jhg050908@gmail.com>
2023-10-18 01:24:33 +0900

    [#53] fix: RegisterFirstUiState - 버튼 초기 상태 변경
    firstBtnState: true -> false

--- app/src/main/java/com/example/dotoring_neoul/ui/register/first/RegisterFirstUiState.kt
+++ app/src/main/java/com/example/dotoring_neoul/ui/register/first/RegisterFirstUiState.kt
@@ -11,18 +11,21 @@ data class RegisterFirstUiState(
     val field: String = "",
     val major: String = "",
 
+
     // BottomSheet OptionList
     val optionFieldList: List<String> = listOf(),
     val optionMajorList: List<String> = listOf(),
 
+
     // BottomSheet ChosenList
     val chosenFieldList: MutableList<String> = mutableListOf(),
     val chosenMajorList: MutableList<String> = mutableListOf(),
+
 
     // 버튼 활성화 조건과 버튼 활성화 여부에 관한 state
     val fillCompanyField: Boolean = false,
     val fillCareerField: Boolean = false,
     val fillJobField: Boolean = false,
     val fillMajorField: Boolean = false,
-    val firstBtnState: Boolean = true
+    val firstBtnState: Boolean = false
 )
