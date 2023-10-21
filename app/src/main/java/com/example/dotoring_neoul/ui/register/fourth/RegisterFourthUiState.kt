5f7a963ecdd036f42c01cdbb0283ce4e48857717
HyeonjiJang <jhg050908@gmail.com>
2023-10-21 16:31:06 +0900

    [#53] update: 회원 가입 네번째 화면 - UiState 멤버 이름 변경
    - mentorIntroduction -> memberIntroduction

--- app/src/main/java/com/example/dotoring_neoul/ui/register/fourth/RegisterFourthUiState.kt
+++ app/src/main/java/com/example/dotoring_neoul/ui/register/fourth/RegisterFourthUiState.kt
@@ -4,6 +4,6 @@ package com.example.dotoring_neoul.ui.register.fourth
  * 회원 가입 네번째 화면 UiState
  */
 data class RegisterFourthUiState(
-    val mentorIntroduction: String = "",
+    val memberIntroduction: String = "",
     val nextButtonState: Boolean = false
 )
